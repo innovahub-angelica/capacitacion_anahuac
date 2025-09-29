f621e1ad446185845f0ae6600a02954d0420e406
civan-ready <civan@readymind.ms>
2025-09-29 09:47:08 -0600

    Validación error

--- test/test/test_module.py
+++ test/test/test_module.py
@@ -1,4 +1,4 @@
 from python_package import module
 
 def test_hello():
-    assert module.hello() == "Hello: asset bundle databricks ci cd github2"
+    assert module.hello() == "Hello,: asset bundle databricks ci cd github2"
